8bd591879914031250c7131660168835d6e7b12a
Ivo Greshev <gresho78@gmail.com>
2020-02-25 12:23:47 +0200

    add firebase

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-
 import "typeface-oswald";
+import firebase from "firebase/app";
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  appId: process.env.REACT_APP_APP_ID
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
