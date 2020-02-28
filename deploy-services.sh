0e37a7d2dae54699a6ba8a752ca9dd1d7223bae1
Ivo Greshev <gresho78@gmail.com>
2020-02-28 12:43:16 +0200

    add texas-holdem game logic and services

---
+++ deploy-services.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#pkr1 is a OpenSSH config shortcut
+
+echo 'deploying services...'
+scp -r ./services/*.json ./services/*.js ./services/lib pkr1:/opt/lightning-poker/
+echo 'installing npm dependencies...'
+ssh pkr1 "cd /opt/lightning-poker && npm install"
