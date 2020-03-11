4588053183a06e5e444b9fd83cc3d0f1f1714517
Ivo Greshev <gresho78@gmail.com>
2020-03-11 13:17:02 +0200

    add PlayerChips

---
+++ src/Player/PlayerChips.js
@@ -0,0 +1,40 @@
+import React, { useContext, useState, useEffect } from "react";
+import { TableContext } from "../Table";
+import { PlayerContext } from "../Table/Players";
+
+function PlayerChips({ position }) {
+  const { coordinates } = useContext(TableContext);
+  const { chips } = useContext(PlayerContext);
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+  const ref = React.createRef();
+  const [x1, y1] = coordinates[position];
+  const [x2, y2] = coordinates["center"];
+  const d = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+  const d2 = 0.7 * d;
+  const left = (d2 * (x1 - x2)) / d + x2;
+  const top = (d2 * (y1 - y2)) / d + y2;
+
+  useEffect(() => {
+    if (ref.current) {
+      let { width, height } = ref.current.getBoundingClientRect();
+      setWidth(Math.round(width));
+      setHeight(Math.round(height));
+    }
+  }, [ref, width, height]);
+
+  return (
+    <div
+      ref={ref}
+      style={{
+        top: top - height / 2,
+        left: left - width / 2
+      }}
+      className="player-chips"
+    >
+      {chips}
+    </div>
+  );
+}
+
+export default PlayerChips;
--- src/Table/Players.js
+++ src/Table/Players.js
@@ -3,6 +3,7 @@ import JoinTable from "./JoinTable";
 import LeaveTable from "./LeaveTable";
 import { TableContext } from "./index";
 import Player from "../Player";
+import PlayerChips from "../Player/PlayerChips";
 
 export const PlayerContext = createContext();
 
@@ -15,6 +16,7 @@ function Players() {
         players[i + 1] ? (
           <PlayerContext.Provider key={i} value={players[i + 1]}>
             <Player position={i + 1} />
+            <PlayerChips position={i + 1} />
           </PlayerContext.Provider>
         ) : (
           <JoinTable key={i} position={i + 1} />
