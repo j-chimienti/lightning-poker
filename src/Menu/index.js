d2b640d731b6b689282b79c5f4c929503382248d
Ivo Greshev <gresho78@gmail.com>
2020-03-25 16:49:04 +0200

    add some info

--- src/Menu/index.js
+++ src/Menu/index.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import Deposit from "../Deposit";
 import Widhtdraw from "../Withdraw";
+import { AppContext } from "../App";
 
 import "./styles.scss";
 
 function Menu() {
+  let { profileId, balance } = useContext(AppContext);
+
   return (
     <aside className="menu">
       <div className="content">
+        <h3>Balance</h3>
+        <h4>{balance} sats</h4>
         <Deposit />
         <Widhtdraw />
+        <div className="title">Account</div>
+        <div className="info">{profileId}</div>
+        <div className="title">Telegram</div>
+        <div className="info">LNPoker</div>
+        <div className="title">CONTACT</div>
+        <div className="info">lightning-poker@protonmail.com</div>
       </div>
     </aside>
   );
