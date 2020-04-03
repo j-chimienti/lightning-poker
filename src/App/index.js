52e9dfe90b24a03927d2f66f0acd764510c51af0
Ivo Greshev <gresho78@gmail.com>
2020-04-03 07:29:45 +0300

    introduce actions

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, createContext } from "react";
+import React, { useEffect, createContext, useReducer } from "react";
 import Menu from "../Menu";
 import Nav from "../Nav";
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -9,6 +9,7 @@ import Games from "../Games";
 import ToggleButton from "./ToggleButton";
 import Table from "../Table";
 import Lobby from "../Lobby";
+import reducer, { initialState } from "./reducer";
 
 import "./styles.scss";
 
@@ -16,6 +17,7 @@ export const AppContext = createContext();
 
 function App() {
   const [user, loading, error] = useAuthState(firebase.auth());
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
     if (loading) {
@@ -43,9 +45,11 @@ function App() {
   return (
     <AppContext.Provider
       value={{
+        state,
         balance,
         profileId: user && user.uid,
-        profileHash: hash
+        profileHash: hash,
+        dispatch
       }}
     >
       <Router>
---
+++ src/App/reducer.js
@@ -0,0 +1,19 @@
+export const initialState = {};
+
+const handlers = [];
+
+export function addHandler(type, fn) {
+  handlers.push([type, fn]);
+}
+
+function reducer(state, action) {
+  let changes = {};
+  for (const [type, fn] of handlers) {
+    if (type === action.type) {
+      Object.assign(changes, fn(action, state, changes));
+    }
+  }
+  return Object.assign({}, state, changes);
+}
+
+export default reducer;
