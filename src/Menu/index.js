9af88bbfb89af0723aa463b17ee03bfb4c898b8a
Ivo Greshev <gresho78@gmail.com>
2020-04-05 07:35:02 +0300

    add poker node info

--- src/Menu/index.js
+++ src/Menu/index.js
@@ -16,7 +16,12 @@ function Menu() {
         <Deposit />
         <Widhtdraw />
         <div className="title">Account</div>
-        <div className="info">{profileId}</div>
+        <div className="info courier">{profileId}</div>
+        <div className="title">Lightning Node</div>
+        <div className="info courier">
+          <span>03ad156742a9a9d0e82e0022f264d6857</span>
+          <span>addfd534955d5e97de4a695bf8dd12af0</span>
+        </div>
         <div className="title">CONTACT</div>
         <div className="info">
           <a href="mailto:lightning-poker@protonmail.com">Mail</a>
