fbe2bbe25e76a59455ed1168726930405ffb05e4
Ivo Greshev <gresho78@gmail.com>
2020-04-11 17:14:35 +0300

    enlarge bet size

--- src/Room/PlayerBet.js
+++ src/Room/PlayerBet.js
@@ -23,7 +23,6 @@ function PlayerBet({ bet, tablePosition }) {
           <defs>
             <filter x="0" y="0" width="1" height="1" id="solid">
               <feFlood floodColor="rgba(0,0,0,0.2)" />
-              <feComposite in="SourceGraphic" operator="xor" />
             </filter>
           </defs>
           <text
@@ -32,7 +31,7 @@ function PlayerBet({ bet, tablePosition }) {
             y={y}
             width={BET_CHIP_STACK_SIZE * 2}
             height={BET_CHIP_STACK_SIZE}
-            fontSize={16}
+            fontSize={18}
             text-anchor={textAnchor}
           >
             {"\u00a0"}
@@ -44,7 +43,7 @@ function PlayerBet({ bet, tablePosition }) {
             y={y}
             width={BET_CHIP_STACK_SIZE * 2}
             height={BET_CHIP_STACK_SIZE}
-            fontSize={16}
+            fontSize={18}
             text-anchor={textAnchor}
           >
             {"\u00a0"}
