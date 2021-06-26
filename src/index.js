de0e8fb174acdb7e4efb881f2ed78828198b57ae
Ivo Greshev <gresho78@gmail.com>
2021-06-26 20:07:01 +0300

    update packages

--- src/index.js
+++ src/index.js
@@ -2,19 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 import "typeface-oswald";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
 firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
-  appId: process.env.REACT_APP_APP_ID
+  appId: process.env.REACT_APP_APP_ID,
 });
 
 ReactDOM.render(<App />, document.getElementsByTagName("main")[0]);
 
-serviceWorker.register();
+reportWebVitals();
+serviceWorkerRegistration.register();
